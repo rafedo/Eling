8215ab85deb13d2167b2b225ec1af29624705351
ReyhanArim <reyhanyulanda12@gmail.com>
2023-03-28 20:15:27 +0700

    modified:   database/migrations/2023_03_25_045942_create_materi_table.php 	modified:   database/migrations/2023_03_25_050034_create_artikel_table.php

--- database/migrations/2023_03_25_050034_create_artikel_table.php
+++ database/migrations/2023_03_25_050034_create_artikel_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nama');
             $table->string('link');
+            $table->int('id_kategori');
             $table->timestamps();
         });
     }
